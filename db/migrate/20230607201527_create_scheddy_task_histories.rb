dae392dc555bc1b42ccbf32e00e8c358cd862828
thomas morgan <tm@iprog.com>
2026-03-23 09:59:10 -0600

    tidy up migrations

--- db/migrate/20230607201527_create_scheddy_task_histories.rb
+++ db/migrate/20230607201527_create_scheddy_task_histories.rb
@@ -1,9 +1,11 @@
 class CreateScheddyTaskHistories < ActiveRecord::Migration[7.0]
   def change
     create_table :scheddy_task_histories do |t|
-      t.string     :name,        null: false, index: {unique: true}
+      t.string     :name,        null: false
       t.datetime   :last_run_at
       t.timestamps
+
+      t.index :name, unique: true
     end
   end
 end
